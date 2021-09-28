1801f30d5b2f820cf37e02a0d0df3c62149d04e5
Matthew Vargas <49174708+Automaton1989@users.noreply.github.com>
2021-09-27 18:00:01 -0700

    Added keyCode events to js file for pacman

--- js/pacman.js
+++ js/pacman.js
@@ -1,23 +1,32 @@
 let pacman = {
-  x: 1,
-  y: 1
+  x: 4,
+  y: 4
 }
 
-let score = 0
-
 let map = [
-  [1, 1, 1, 1, 1, 1],
-  [1, 5, 2, 2, 2, 1],
-  [1, 2, 2, 2, 2, 1],
-  [1, 1, 1, 1, 1, 1]
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 3, 3, 3, 2, 3, 3, 3, 3, 1],
+  [1, 3, 3, 3, 2, 3, 3, 3, 3, 1],
+  [1, 3, 3, 3, 3, 3, 3, 3, 3, 1],
+  [1, 3, 3, 3, 5, 3, 3, 3, 3, 1],
+  [1, 3, 3, 3, 3, 3, 3, 3, 3, 1],
+  [1, 3, 3, 3, 3, 3, 3, 3, 3, 1],
+  [1, 3, 3, 3, 3, 3, 3, 3, 3, 1],
+  [1, 3, 3, 3, 3, 3, 3, 3, 3, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ]
+
+var score = 0
 
 var board = document.getElementById('pacman-world')
 
 var button = document.getElementById('play').addEventListener('click', playGame)
 
+var newScore = document.getElementById('score').style.display = 'none'
+
 function playGame() {
   let remove = document.getElementById('play').style.display = 'none'
+  newScore = document.getElementById('score').style.display = 'block'
   drawWorld()
 }
 
@@ -42,5 +51,69 @@ function drawWorld() {
       }
     }
     board.innerHTML += '<br>'
+  }
+}
+
+document.onkeydown = function (event) {
+  /* console.log(event) */
+  /* MOVE LEFT */
+  if (event.keyCode === 37) {
+    if (map[pacman.y][pacman.x - 1] === 2) {
+      console.log('COIN!')
+      newScore = document.getElementById('score').innerHTML = ''
+      score += 10
+      newScore = document.getElementById('score').innerHTML = score
+    }
+    if (map[pacman.y][pacman.x - 1] !== 1) {
+      map[pacman.y][pacman.x] = 3
+      pacman.x = pacman.x - 1
+      map[pacman.y][pacman.x] = 5
+      drawWorld()
+    }
+  }
+  /* MOVE RIGHT */
+  if (event.keyCode === 39) {
+    if (map[pacman.y][pacman.x + 1] === 2) {
+      console.log('COIN!')
+      newScore = document.getElementById('score').innerHTML = ''
+      score += 10
+      newScore = document.getElementById('score').innerHTML = score
+    }
+    if (map[pacman.y][pacman.x + 1] !== 1) {
+      map[pacman.y][pacman.x] = 3
+      pacman.x = pacman.x + 1
+      map[pacman.y][pacman.x] = 5
+      drawWorld()
+    }
+  }
+  /* MOVE UP */
+  if (event.keyCode === 38) {
+    if (map[pacman.y - 1][pacman.x] === 2) {
+      console.log('COIN!')
+      newScore = document.getElementById('score').innerHTML = ''
+      score += 10
+      newScore = document.getElementById('score').innerHTML = score
+    }
+    if (map[pacman.y - 1][pacman.x] !== 1) {
+      map[pacman.y][pacman.x] = 3
+      pacman.y = pacman.y - 1
+      map[pacman.y][pacman.x] = 5
+      drawWorld()
+    }
+  }
+  /* MOVE DOWN */
+  if (event.keyCode === 40) {
+    if (map[pacman.y + 1][pacman.x] === 2) {
+      console.log('COIN!')
+      newScore = document.getElementById('score').innerHTML = ''
+      score += 10
+      newScore = document.getElementById('score').innerHTML = score
+    }
+    if (map[pacman.y + 1][pacman.x] !== 1) {
+      map[pacman.y][pacman.x] = 3
+      pacman.y = pacman.y + 1
+      map[pacman.y][pacman.x] = 5
+      drawWorld()
+    }
   }
 }
